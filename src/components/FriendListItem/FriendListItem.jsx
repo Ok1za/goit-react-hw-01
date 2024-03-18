be540d77a5f1e278584b9b6acbdcc53ca597fb18
Ok1za <oksanaprescott@gmail.com>
2024-03-18 23:27:35 +0200

    done t1 t2 t3

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import styles from '../FriendListItem/FriendListItem.module.css';
+
+export default function FriendListItem({ avatar, name, isOnline }) {
+    return (
+        <div className={styles.friendListItem}>
+            <img
+                src={avatar}
+                alt="Avatar"
+                className={styles.friendAvatar}
+            />
+            <p className={styles.friendName}>{name}</p>
+            <p className={isOnline ? styles.online : styles.offline}>{isOnline ? 'Online' : 'Offline'}</p>
+        </div>
+    );
+}
+
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import styles from '../Profile/Profile.module.css';
+
+export default function Profile({ name, tag, location, image, stats }) {
+    const { followers, views, likes } = stats;
+    
+    return (
+    <div className={styles["container-profile"]}> 
+        <div className={styles["profile-div"]}>
+            <img
+                src={image}
+                alt="User avatar"
+                className={styles["profile-avatar"]} 
+            />
+            <p className={styles["profile-name"]}>{name}</p> 
+            <p className={styles["profile-tag"]}>@{tag}</p> 
+            <p className={styles["profile-location"]}>{location}</p> 
+        </div>
+
+        <ul className={styles["profile-stats"]}> 
+            <li className={styles["profile-li"]}>
+                <span>Followers </span>
+                <span>{followers}</span>
+            </li>
+            <li className={styles["profile-li"]}>
+                <span>Views </span>
+                <span>{views}</span>
+            </li>
+            <li className={styles["profile-li"]}>
+                <span>Likes </span>
+                <span>{likes}</span>
+            </li>
+        </ul>
+    </div>
+    );
+}
